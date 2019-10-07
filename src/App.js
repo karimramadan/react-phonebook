3b11cb16a420441cd7291abc7ebf2addb22a3152
Karim ramadan <karim.ramadan@gmail.com>
2019-10-08 00:20:46 +0700

    Add firebase auth to the mix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import Main from "./components/Main"
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Contacts from "./components/Contacts";
 import './css/normalize.css';
 import './css/App.css';
 
 class App extends React.Component{
   render(){
     return (
-      <div id="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div id="App">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/contacts' component={Contacts} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
